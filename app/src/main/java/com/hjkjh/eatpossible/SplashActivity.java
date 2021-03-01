def9479f701db6f4e21d4b0b6505d8d0809fc8c9
unknown <ptjh0309@hanmail.net>
2021-03-01 22:25:48 +0900

    Upload initialize EatPossible_Android Application

---
+++ app/src/main/java/com/hjkjh/eatpossible/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.hjkjh.eatpossible;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+}
